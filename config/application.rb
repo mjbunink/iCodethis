bda51fbaf96c53a1a866848181cf3cdbdc9420aa
mjbunink <mjbunink@git-kraken-reletavity-5.com>
2025-01-14 10:33:14 +0100

    Turned off dbase test for Github

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,10 @@ require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
 # require "active_job/railtie"
-require "active_record/railtie"
+
+# Turn off dbase functionality
+# require "active_record/railtie"
+
 # require "active_storage/engine"
 require "action_controller/railtie"
 # require "action_mailer/railtie"
